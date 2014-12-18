65bf043e17cc042eacaa46651413dc4a674dcc4c
ramuchembeti <ramu.chembeti@gmail.com>
2014-12-18 23:19:14 +0530

    Update Hello.java
    v3

--- Hello/src/com/speroware/services/test/Hello.java
+++ Hello/src/com/speroware/services/test/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println("................Hello GIT.....");
 		System.out.println("...... test1 ");
 		System.out.println(".....test2......");
+			System.out.println(".....test3......");
 	}
 
 }
