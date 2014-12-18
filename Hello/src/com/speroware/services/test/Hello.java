46281ec472dc59bc300e82c445b90ed4fe2ca529
ramu seanergy <ramu@speroware.com>
2014-12-18 23:17:38 +0530

    2nd version

--- Hello/src/com/speroware/services/test/Hello.java
+++ Hello/src/com/speroware/services/test/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("................Hello GIT.....");
 		System.out.println("...... test1 ");
+		System.out.println(".....test2......");
 	}
 
 }
